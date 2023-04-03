ded9e328126bf34501359976c81576b29c8e2cee
Tucznik2 <f3kula@gmail.com>
2023-04-03 09:22:03 +0200

    initial commit

---
+++ src/test/java/com/filip/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filip.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
